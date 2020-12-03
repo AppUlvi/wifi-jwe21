e5fc167fa1ee2e6bd04d6175c3f1d55a86b73638
Ulvi Ulu <appulvi@icloud.com>
2020-12-03 21:19:37 +0100

    add code

--- JS/js/03anweisungen.js
+++ JS/js/03anweisungen.js
@@ -52,3 +52,29 @@ if ((1 == 1) || (2 > 1 && 4 == 6))
 else
     console.log(false);
 // true
+
+if ('Name'.length == 4)
+    console.log(true);
+else
+    console.log(false);
+// true
+
+if ('Name'.indexOf('o') == -1)
+    console.log(true);
+else
+    console.log(false);
+// true
+
+
+let vorname = 'Qendrim';
+
+switch (vorname) {
+    case 'Roland':
+        console.log('Ich bin Netflix-Fan');
+        break;
+    case 'Qendrim':
+        console.log('Ich habe coding für mich entdeckt!');
+        break;
+    default:
+        console.log("What");
+}
