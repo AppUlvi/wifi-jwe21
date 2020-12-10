01beb08154f41e64566013b1e26c89e8878df3c1
Ulvi Ulu <appulvi@icloud.com>
2020-12-10 17:54:50 +0100

    change checkMyInput add code

--- JS/js/02functions.js
+++ JS/js/02functions.js
@@ -1,14 +1,18 @@
 let userNameFromDatabase = 'Roland';
 
-function isInputString(input) {
-    if (typeof input == 'string' && input.length >= 2 && isNaN(parseInt(input)))
+function checkMyInput(input) {
+    if (typeof input == 'string' && input.length >= 2) {
+        for (i = 0; i < input.length; i++) {
+            if (!(isNaN(parseInt(input.charAt(i)))))
+                return false;
+        } //checks if there is a number in string
         return true;
-    else
+    } else
         return false;
 }
 
 function sayMyName(name) {
-    if (isInputString(name))
+    if (checkMyInput(name))
         console.log('Your name is ' + name);
     else
         console.log(name + ' is not a Name.');
