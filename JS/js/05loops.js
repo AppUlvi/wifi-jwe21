ada545b9798e06e6b3e9918ba3a67792e977e3c7
Ulvi Ulu <appulvi@icloud.com>
2020-12-10 19:37:11 +0100

    small fix

--- JS/js/05loops.js
+++ JS/js/05loops.js
@@ -4,20 +4,20 @@ let namen = [
     'Ulvi'
 ];
 
-for (let i = 0; i < 3; i++) {
-    // console.log('hey ', i);
-    // let html = '<img src="https://placehold.it/300x200?text=' + namen[i] + '">';
-    // console.log(html);
-    // document.getElementById('gallery').innerHTML += html;
-}
+// for (let i = 0; i < 3; i++) {
+// console.log('hey ', i);
+// let html = '<img src="https://placehold.it/300x200?text=' + namen[i] + '">';
+// console.log(html);
+// document.getElementById('gallery').innerHTML += html;
+// }
 
-namen.forEach(
-    //     function (elm, i) {
-    //         console.log(elm);
-    //         let html = '<img src="https://placehold.it/300x200?text=' + elm + '">';
-    //         document.getElementById('gallery').innerHTML += html;
-    //     }
-);
+// namen.forEach(
+//     function (elm, i) {
+//         console.log(elm);
+//         let html = '<img src="https://placehold.it/300x200?text=' + elm + '">';
+//         document.getElementById('gallery').innerHTML += html;
+//     }
+// );
 
 namen.forEach(elm => {
     console.log(elm);
