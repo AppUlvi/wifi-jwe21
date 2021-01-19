c32e53282351323536488fdbad1e6acaf3eb1788
Ulvi Ulu <appulvi@icloud.com>
2021-01-19 21:41:15 +0100

    fix

--- JS/01uebungen/js/09scrollPosition.js
+++ JS/01uebungen/js/09scrollPosition.js
@@ -1,32 +1,39 @@
+// WIFI
+// Ulvi Ulu
+// =============================================================================
 
+let scrollPos = Math.floor($(document).scrollTop());
 
 $(window).scroll(function () {
     let scrollPos = $(document).scrollTop();
     let scrollBox = $('#scrollBox');
 
-    console.log(scrollPos);
+    // console.log(scrollPos);
 
     scrollBox.html(scrollPos);
 
 
     if (scrollPos > 400) {
         scrollBox.addClass('show');
+        if (scrollPos > 1000) {
+            scrollBox.addClass('show');
+            scrollBox.css({
+                'background-color': 'green',
+                'color': 'black'
+            });
+        } else {
+            scrollBox.css({
+                'background-color': '',
+                'color': ''
+            });
+        }
+
     } else {
         scrollBox.removeClass('show');
     }
 
-    if (scrollPos > 1000) {
-        scrollBox.css({
-            'background-color': 'green',
-            'color': 'black'
-
-        });
-    } else {
-        scrollBox.css({
-            'background-color': '',
-            'color': ''
-
-        });
-    }
+    scrollBox.css({
+        'transform': 'translateY(' + scrollPos + 'px)'
+    });
 
 });
