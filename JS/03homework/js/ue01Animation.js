0fa05fd17bf1480d02b6c3d76620b5dddf123bc8
Ulvi Ulu <appulvi@icloud.com>
2021-01-24 11:13:00 +0100

    add comments shorter css

--- JS/03homework/js/ue01Animation.js
+++ JS/03homework/js/ue01Animation.js
@@ -2,10 +2,16 @@
 // Ulvi Ulu
 // =============================================================================
 
-let box = $('.box');
-let scrollPos = 0;
+// main variables
 let scrollBox = $('#scrollBox');
+let box = $('.box');
+let button = $('.btn');
 
+// variables for scroll position
+let scrollPos = 0;
+
+// writes scrollposition into scrollbox 
+// changes the box to a circle depending on the scrollposition
 $(window).scroll(function () {
     scrollPos = Math.floor($(document).scrollTop());
     scrollBox.html('<p>' + scrollPos + '</p>');
@@ -19,32 +25,33 @@ $(window).scroll(function () {
             'border-radius': '0%',
         });
     }
-
 });
 
-let button = $('.btn');
+// variables for click events
 let animCompl = false;
-let cnt = 0;
+let cnt = -1;
 
+// shows the box after the first click, sets animCompl to true and sets the counter
+// else: changes text of button to count and counts 1 up after every click
 button.click(function (e) {
 
-    if (cnt === 0) {
-        cnt++;
+    if (cnt === -1) {
         $('.box.hidden').removeClass('hidden');
+        cnt++
 
         box.animate({
             width: '+=200',
             height: '+=200'
-
         }, 2000, 'linear', function () {
             // Animation complete
             animCompl = true;
-            box.html('<p>' + (cnt - 1) + '</p>');
+            box.html('<p>' + cnt + '</p>');
             button.html('<p>Count</p>');
         });
+
     } else if (animCompl) {
         cnt++;
-        box.html('<p>' + (cnt - 1) + '</p>');
+        box.html('<p>' + cnt + '</p>');
     }
 
 });
