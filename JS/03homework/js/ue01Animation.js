15069a6e657aa66b2449c19f9660879aaa1e27d9
Ulvi Ulu <appulvi@icloud.com>
2021-01-26 19:23:35 +0100

    update

--- JS/03homework/js/ue01Animation.js
+++ JS/03homework/js/ue01Animation.js
@@ -7,8 +7,9 @@ let scrollBox = $('#scrollBox');
 let box = $('.box');
 let button = $('.btn');
 
-// variables for scroll position
+// variables for scroll
 let scrollPos = 0;
+let calculateRadius = 0;
 
 // writes scrollposition into scrollbox 
 // changes the box to a circle depending on the scrollposition
@@ -17,12 +18,21 @@ $(window).scroll(function () {
     scrollBox.html('<p>' + scrollPos + '</p>');
 
     if (scrollPos >= 300) {
-        console.log('scrollpos: over 300')
-        box.css({
-            'border-radius': ((scrollPos - 299) / 15) + '%',
-        });
+        // console.log('scrollpos: over 300') //DEBUG
+        calculateRadius = (scrollPos - 299) / 15;
+
+        if (calculateRadius > 50) {
+            box.css({
+                'border-radius': '50%',
+            });
+        } else {
+            box.css({
+                'border-radius': calculateRadius + '%',
+            });
+        }
+
     } else {
-        console.log('scrollpos: under 300')
+        // console.log('scrollpos: under 300') //DEBUG
         box.css({
             'border-radius': '0%',
         });
@@ -36,7 +46,7 @@ let cnt = -1;
 // shows the box after the first click, sets animCompl to true and sets the counter
 // else: changes text of button to count and counts 1 up after every click
 button.click(function (e) {
-    console.log('button pressed, count: ' + (cnt + 1))
+    // console.log('button pressed, count: ' + (cnt + 1)) //DEBUG
 
     if (cnt === -1) {
         cnt++
@@ -44,7 +54,9 @@ button.click(function (e) {
 
         box.animate({
             width: '+=200',
-            height: '+=200'
+            height: '+=200',
+            top: '-=100',
+            left: '-=100'
         }, 2000, 'linear', function () {
             // Animation complete
             animCompl = true;
