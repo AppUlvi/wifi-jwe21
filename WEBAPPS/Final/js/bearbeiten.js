6638c13d1f6bc076e300061f081d59621d19c0da
Ulvi Ulu <appulvi@icloud.com>
2021-03-26 03:48:05 +0100

    version 1.1

--- WEBAPPS/Final/js/bearbeiten.js
+++ WEBAPPS/Final/js/bearbeiten.js
@@ -26,10 +26,31 @@ function getParamFromURL(param) {
     return urlParams.get(param);
 }
 
+// fills notification div with a message and a link to go back to index
+// sets a warning class to the p tag if wanted
+function onNotification(message, warn = false) {
+    const div = document.querySelector('#notification');
+    div.innerHTML = "";
+
+    const p = document.createElement('p');
+    p.textContent = message;
+    if (warn) {
+        p.classList.add('warning');
+    }
+
+    const link = document.createElement('a');
+    link.textContent = 'Zurueck zur Uebersicht';
+    link.href = 'index.html';
+
+    div.appendChild(p);
+    div.appendChild(link);
+}
+
 // =============================================================================
 
 function getTower() {
     const id = getParamFromURL('id');
+
     fetch(`https://test.sunbeng.eu/api/towers/${id}`)
         .then(function (response) {
             if (response.ok) {
@@ -38,8 +59,8 @@ function getTower() {
                 throw new Error("Error getting tower with the id: " + id);
             }
         }).then(function (tower) {
-            const towerDetails = document.querySelector('#tower-details');
-            towerDetails.innerHTML = "";
+            const towerDetailsDiv = document.querySelector('#tower-details');
+            towerDetailsDiv.innerHTML = "";
 
             for (const prop in tower) {
                 const labelInputDiv = document.createElement('div');
@@ -62,6 +83,7 @@ function getTower() {
                         break;
                     case "range":
                         label.textContent = 'Reichweite:';
+                        input.setAttribute('type', 'number');
                         break;
                     case "lat":
                         label.textContent = 'Breitengrad:';
@@ -93,13 +115,12 @@ function getTower() {
                 if (label.textContent !== "") {
                     labelInputDiv.appendChild(label);
                     labelInputDiv.appendChild(input);
-                    towerDetails.appendChild(labelInputDiv);
+                    towerDetailsDiv.appendChild(labelInputDiv);
                 }
 
                 // DEBUG
                 // console.log(prop);
             }
-            // return { name: tower.name, range: parseInt(tower.range), is5GEnabled: tower.is5GEnabled }
         }).catch(function (error) {
             onError(error);
             // console.log(error.message);
@@ -108,47 +129,104 @@ function getTower() {
 
 // =============================================================================
 
-// save new values
-function postTower() {
-    const id = getParamFromURL('id');
+// Async functions:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+// https://dmitripavlutin.com/javascript-fetch-async-await/
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
 
+// gets name, range and 5g info from tower and returns it
+// async keyword to use await in the function
+async function getTowerDetails() {
+    const id = getParamFromURL('id');
+    let towerDetails = {};
+    // await causes to pause your code until the async function returns the result
+    await fetch(`https://test.sunbeng.eu/api/towers/${id}`)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Error getting tower with the id: " + id);
+            }
+        }).then(function (tower) {
+            // fills the towerDetails object
+            towerDetails = { name: tower.name, range: parseInt(tower.range), is5GEnabled: tower.is5GEnabled };
+        }).catch(function (error) {
+            onError(error);
+        });
+    // returns the towerDetails object
+    return towerDetails;
+}
+
+// saves new values with POST
+async function postTower() {
+    const id = getParamFromURL('id');
+    //  writes the current values into the tower object
     const towerDetails = {
         name: document.querySelector('#input-name').value,
         range: parseInt(document.querySelector('#input-range').value),
         is5GEnabled: document.querySelector('#input-is5GEnabled').checked
     };
 
-    fetch(`https://test.sunbeng.eu/api/towers/${id}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(towerDetails),
-    }).then(function (response) {
-        if (response.ok) {
-            const div = document.querySelector('#notification');
-            div.innerHTML = "";
+    // waits until tower gets filled
+    const tower = await getTowerDetails();
 
-            const p = document.createElement('p');
-            p.textContent = `Die Aenderung am Sendemasten wurde erfolgreich gespeichert.`;
-
-            const link = document.createElement('a');
-            link.textContent = 'Zurueck zur Uebersicht';
-            link.href = 'index.html';
-
-            div.appendChild(p);
-            div.appendChild(link);
-
-            getTower();
-
-        } else {
-            throw new Error("Post ist fehlgeschlagen");
-        }
-    }).catch(function (error) {
-        onError(error);
-    });
-
+    // if the current details are the same as the previous GET details warn user 
+    // else use fetch POST to change the values
+    if (tower.name === towerDetails.name && tower.range === towerDetails.range && tower.is5GEnabled === towerDetails.is5GEnabled) {
+        onNotification("Es wurde nichts verändert", true);
+    } else if (isNaN(towerDetails.range)) {
+        onNotification("Bitte eine gueltige Zahl bei der Reichweite eingegeben", true);
+    } else {
+        fetch(`https://test.sunbeng.eu/api/towers/${id}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(towerDetails),
+        }).then(function (response) {
+            if (response.ok) {
+                onNotification('Die Aenderung am Sendemasten wurde erfolgreich gespeichert.');
+                // refreshes values
+                getTower();
+                const errorDiv = document.querySelector('#error');
+                errorDiv.innerHTML = "";
+            } else {
+                throw new Error("Post ist fehlgeschlagen");
+            }
+        }).catch(function (error) {
+            onError(error);
+        });
+    }
 }
+
+// // without async functions
+// function postTower() {
+//     const id = getParamFromURL('id');
+//     //  writes the current values into the tower object
+//     const towerDetails = {
+//         name: document.querySelector('#input-name').value,
+//         range: parseInt(document.querySelector('#input-range').value),
+//         is5GEnabled: document.querySelector('#input-is5GEnabled').checked
+//     };
+
+//     fetch(`https://test.sunbeng.eu/api/towers/${id}`, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(towerDetails),
+//     }).then(function (response) {
+//         if (response.ok) {
+//             onNotification('Die Aenderung am Sendemasten wurde erfolgreich gespeichert.');
+//             // refreshes values
+//             getTower();
+//         } else {
+//             throw new Error("Post ist fehlgeschlagen");
+//         }
+//     }).catch(function (error) {
+//         onError(error);
+//     });
+// }
 
 // =============================================================================
 
